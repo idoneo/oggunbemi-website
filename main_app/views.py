0976ec85178ae911608c4397e24d744e529f1e6a
Maria Concordia <12888141+idoneo@users.noreply.github.com>
2024-08-10 21:26:55 -0400

    Update views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
+    return render(request, 'main_app/home.html')
+
+
+def landing_page(request):
     return render(request, 'main_app/landing.html')
