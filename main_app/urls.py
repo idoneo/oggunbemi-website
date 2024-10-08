fe4fdf1779cc95040d27bddc6519a735af27e774
bemi151 <bemi151@gmail.com>
2024-10-08 11:00:54 -0400

    adding statci, ignoring .jpg and mpv4

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing_page, name='landing_page'),
+    
+]
+
+urlpatterns = [
+    path('', views.home, name='home'), #enter_home
+    path('es/', views.home_spanish, name='home_spanish'),#enter_home_sp
+    path('plants/', views.plants, name='plants'), #plants_home
+    path('plants_es/', views.plants_es, name='plants_es'), #plants_home
+    path('original/', views.landing_page, name='landing_page'), #original landing page
+    path('metalworks/', views.metalworks, name='metalworks'),#metalworks_home
+    path('es_metalworks/', views.metalworks_es, name='metalworks_es'),#metalworks_home_sp
+    path('enter_home/', views.enter_home, name='enter_home'),
+    path('metalworks_samples/', views.metalworks_samples, name='metalworks_samples'),
+    path('metalworks_samples_es/', views.metalworks_samples_es, name='metalworks_samples_es'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,45 @@
+
 from django.shortcuts import render
+from django.conf import settings
 
 def home(request):
-    return render(request, 'main_app/home.html')
+    context = {
+        'STATIC_URL': settings.STATIC_URL,
+    }
+    return render(request, 'main_app/enter_home.html', context)
 
+def home_spanish(request):
+    return render(request, 'main_app/enter_home_sp.html')
+
+def plants(request):
+    return render(request, 'main_app/plants_home.html')
+
+def plants_es(request):
+    return render(request, 'main_app/plants_home_es.html')  
+
+def metalworks(request):
+    return render(request, 'main_app/metalworks_home.html')
+
+def metalworks_es(request):
+    return render(request, 'main_app/metalworks_home_sp.html')
+
+#def home(request):
+    #return render(request, 'main_app/home.html') #original home page
 
 def landing_page(request):
-    return render(request, 'main_app/landing.html')
+    return render(request, 'main_app/landing.html') #original landing page
+
+def enter_home(request):
+    context = {
+        'photo_range': range(1, 15)  # Creates a range of numbers from 1 to 14
+    }
+    return render(request, 'main_app/enter_home.html', context)
+
+def enter_home(request):
+    return render(request, 'main_app/enter_home.html')
+
+def metalworks_samples(request):
+    return render(request, 'main_app/metalworks_samples.html')
+
+def metalworks_samples_es(request):
+    return render(request, 'main_app/metalworks_samples_es.html')
